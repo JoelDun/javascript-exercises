267062d8053f454763888275216c0505a20ae15b
Joel.Dun <joel.dun@pm.me>
2022-12-31 04:42:03 -0500

    Complete reverseString test 1

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
 const repeatString = function() {
+let number = 3
+let string = "hey"
 
+  for (let i = 0; i < number; i++) {
+  string += string + string
+  return string
+  }
 };
 
 // Do not edit below this line
